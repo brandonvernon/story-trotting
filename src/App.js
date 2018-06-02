4db8363e6829b1448f7e565f1a5b73fa06429589
Tracy <tmullen@clearblade.com>
2018-06-02 12:40:36 -0500

    add map

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import React, { Component } from "react";
 import "./App.css";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
+
+const MyMapComponent = withScriptjs(
+  withGoogleMap(props => (
+    <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+      {props.isMarkerShown && (
+        <Marker position={{ lat: -34.397, lng: 150.644 }} />
+      )}
+    </GoogleMap>
+  ))
+);
 
 class App extends Component {
   render() {
@@ -8,6 +24,13 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Story Trotting</h1>
         </header>
+        <MyMapComponent
+          isMarkerShown
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
         <p className="App-intro">Container for our app.</p>
       </div>
     );
