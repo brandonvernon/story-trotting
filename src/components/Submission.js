bd72846f188880d8398f7446a84e7ccecfb668f1
Brandon Vernon <brandontvernon@gmail.com>
2018-06-02 16:40:18 -0500

    Started styling

--- src/components/Submission.js
+++ src/components/Submission.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-//import './Submission.css';
+import './Submission.css';
 
 class Submission extends Component {
   constructor(props) {
@@ -21,57 +21,77 @@ class Submission extends Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Headline
-          <input type="text" value={this.state.name} onChange={this.handleChange} />
-        </label>
-        <label>
-          Description
-          <input type="text" value={this.state.description} onChange={this.handleChange} />
-        </label>
-        <label>
-          Location Name
-          <input type="text" value={this.state.location_name} onChange={this.handleChange} />
-        </label>
-        <label>
-          Address
-          <input type="text" value={this.state.address} onChange={this.handleChange} />
-        </label>
-        <label>
-          Tags
-          <input type="text" value={this.state.tags} onChange={this.handleChange} />
-        </label>
-        <label>
-          Collection
-          <input type="text" value={this.state.collection} onChange={this.handleChange} />
-        </label>
-        <label>
-          Latitude
-          <input type="text" value={this.state.lat} onChange={this.handleChange} />
-        </label>
-        <label>
-          Longitude
-          <input type="text" value={this.state.long} onChange={this.handleChange} />
-        </label>
-        <label>
-          Time
-          <input type="text" value={this.state.timestamp} onChange={this.handleChange} />
-        </label>
-        <label>
-          Time End
-          <input type="text" value={this.state.timestamp_end} onChange={this.handleChange} />
-        </label>
-        <label>
-          Sources
-          <input type="text" value={this.state.sources} onChange={this.handleChange} />
-        </label>
-        <label>
-          Your Name
-          <input type="text" value={this.state.owner} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <div className='container'>
+        <form className='form' onSubmit={this.handleSubmit}>
+          <div className='row'>
+            <label>
+              Headline
+              <input type="text" value={this.state.name} onChange={this.handleChange} />
+            </label>
+            <label>
+              Description
+              <input type="text" value={this.state.description} onChange={this.handleChange} />
+            </label>
+          </div>
+          <div className='row'>
+            <label>
+              Location Name
+              <input type="text" value={this.state.location_name} onChange={this.handleChange} />
+            </label>
+            <label>
+              Address
+              <input type="text" value={this.state.address} onChange={this.handleChange} />
+            </label>
+          </div>
+          <div className='row'>
+            <label>
+              Latitude
+              <input type="text" value={this.state.lat} onChange={this.handleChange} />
+            </label>
+            <label>
+              Longitude
+              <input type="text" value={this.state.long} onChange={this.handleChange} />
+            </label>
+          </div>
+          <div className='row'>
+            <label>
+              Drop Pin
+              <input type="text" value={this.state.drop_pin} onChange={this.handleChange} />
+            </label>
+            <label>
+              Tags
+              <input type="text" value={this.state.tags} onChange={this.handleChange} />
+            </label>
+          </div>
+          <div className='row'>
+            <label>
+              Collection
+              <input type="text" value={this.state.collection} onChange={this.handleChange} />
+            </label>
+            <label>
+              Time
+              <input type="text" value={this.state.timestamp} onChange={this.handleChange} />
+            </label>
+          </div>
+          <div className='row'>
+            <label>
+              Time End
+              <input type="text" value={this.state.timestamp_end} onChange={this.handleChange} />
+            </label>
+            <label>
+              Sources
+              <input type="text" value={this.state.sources} onChange={this.handleChange} />
+            </label>
+          </div>
+          <div className='row'>
+            <label>
+              Your Name
+              <input type="text" value={this.state.owner} onChange={this.handleChange} />
+            </label>
+          </div>
+            <input type="submit" value="Submit" />
+        </form>
+      </div>
     );
   }
 }
