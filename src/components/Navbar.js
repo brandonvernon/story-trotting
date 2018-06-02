81938ddd5cf39cae89733202e0eaf2f484a24d0c
Kenny Vo <kennyvo91@gmail.com>
2018-06-02 15:00:50 -0500

    responsive navbars for mobile and web

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,41 +5,65 @@ import {
   NavItem,
   FormGroup,
   FormControl,
-  Button,
   MenuItem,
   NavDropdown
 } from 'react-bootstrap';
+import MediaQuery from 'react-responsive';
 import './Navbar.css';
 
 class NavbarHeader extends Component {
   render() {
     return (
-      <Navbar collapseOnSelect>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <a href="#brand">Story Strotting</a>
-          </Navbar.Brand>
-          <Navbar.Toggle/>
-          <Navbar.Form pullLeft>
-            <FormGroup>
-              <FormControl type="text" placeholder="Search"/>
-            </FormGroup>{' '} {/* <Button type="submit">Submit</Button> */}
-          </Navbar.Form>
-        </Navbar.Header>
-        <Navbar.Collapse className="nav-sandwich">
-          <Nav>
+      <div>
+      <MediaQuery minWidth={450} maxWidth={1500}>
+        <Navbar collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#brand">Story Strotting</a>
+            </Navbar.Brand>
+            <Navbar.Toggle/>
+            <Navbar.Form pullLeft>
+              <FormGroup>
+                <FormControl type="text" placeholder="Search"/>
+              </FormGroup>{' '} {/* <Button type="submit">Submit</Button> */}
+            </Navbar.Form>
+          </Navbar.Header>
+          <Navbar.Collapse className="nav-sandwich">
+            <Nav>
+              <NavItem eventKey={1} href="#">
+                List View
+              </NavItem>
+            </Nav>
+            <Nav pullRight>
+              <NavDropdown eventKey={3} title="Actions" id="basic-nav-dropdown">
+                <MenuItem eventKey={3.1}>Add a Story</MenuItem>
+                <MenuItem eventKey={3.2}>Filters</MenuItem>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </MediaQuery>
+      <MediaQuery maxWidth={450}>
+        <Navbar.Form pullLeft>
+          <FormGroup>
+            <FormControl type="text" placeholder="Search"/>
+        </FormGroup>{' '} {/* <Button type="submit">Submit</Button> */}
+      </Navbar.Form>
+        <Navbar fixedBottom>
+          <Nav pullLeft>
             <NavItem eventKey={1} href="#">
               List View
             </NavItem>
+            <NavItem eventKey={1} href="#">
+              Filters
+            </NavItem>
+            <NavItem eventKey={1} href="#">
+              +Add Story
+            </NavItem>
           </Nav>
-          <Nav pullRight>
-            <NavDropdown eventKey={3} title="Actions" id="basic-nav-dropdown">
-              <MenuItem eventKey={3.1}>Add a Story</MenuItem>
-              <MenuItem eventKey={3.2}>Filters</MenuItem>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+        </Navbar>
+      </MediaQuery>
+    </div>
     )
   }
 }
