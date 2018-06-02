3bac3ad30d0c19f4481b8a6335e7281233b23d27
Tracy <tmullen@clearblade.com>
2018-06-02 11:54:06 -0500

    remove

--- src/App.js
+++ src/App.js
@@ -1,19 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
 
-import { GoogleMap, Marker } from "react-google-maps";
-
-const MyMapComponent = props => (
-  <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
-    {props.isMarkerShown && (
-      <Marker position={{ lat: -34.397, lng: 150.644 }} />
-    )}
-  </GoogleMap>
-);
-
-// <MyMapComponent isMarkerShown />// Map with a Marker
-// <MyMapComponent isMarkerShown={false} />// Just only Map
-
 class App extends Component {
   render() {
     return (
@@ -21,7 +8,6 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Story Trotting</h1>
         </header>
-        <MyMapComponent isMarkerShown />
         <p className="App-intro">Container for our app.</p>
       </div>
     );
