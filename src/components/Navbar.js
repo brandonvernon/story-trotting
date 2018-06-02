2b3df9ccb5779bf54b726bd44832f788959aa381
Kenny Vo <kennyvo91@gmail.com>
2018-06-02 14:23:54 -0500

    adjusted navbar view for web browser

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,42 @@
-import React, { Component } from 'react';
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import React, {Component} from 'react';
+import {
+  Navbar,
+  Nav,
+  NavItem,
+  FormGroup,
+  FormControl,
+  Button,
+  MenuItem,
+  NavDropdown
+} from 'react-bootstrap';
 import './Navbar.css';
-
 
 class NavbarHeader extends Component {
   render() {
     return (
-      <Navbar collapseOnSelect fixedTop>
+      <Navbar collapseOnSelect>
         <Navbar.Header>
-          <Navbar.Toggle />
+          <Navbar.Brand>
+            <a href="#brand">Story Strotting</a>
+          </Navbar.Brand>
+          <Navbar.Toggle/>
+          <Navbar.Form pullLeft>
+            <FormGroup>
+              <FormControl type="text" placeholder="Search"/>
+            </FormGroup>{' '} {/* <Button type="submit">Submit</Button> */}
+          </Navbar.Form>
         </Navbar.Header>
-        <Navbar.Collapse>
+        <Navbar.Collapse className="nav-sandwich">
           <Nav>
             <NavItem eventKey={1} href="#">
               List View
             </NavItem>
-            <NavItem eventKey={2} href="#">
-              Filters
-            </NavItem>
-            <NavItem eventKey={3} href="#">
-              Add Story
-            </NavItem>
+          </Nav>
+          <Nav pullRight>
+            <NavDropdown eventKey={3} title="Actions" id="basic-nav-dropdown">
+              <MenuItem eventKey={3.1}>Add a Story</MenuItem>
+              <MenuItem eventKey={3.2}>Filters</MenuItem>
+            </NavDropdown>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
