052800baaf48e4418660ae5e5ea18d523f85a972
David <capndavet@gmail.com>
2018-06-03 14:38:25 -0500

    final commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
           isMarkerShown
           googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
           loadingElement={<div style={{ height: `100%` }} />}
-          containerElement={<div style={{ height: `75vh` }} />}
+          containerElement={<div style={{ height: `90vh`, marginTop: '-20px' }} />}
           mapElement={<div style={{ height: `100%` }} />}
         />
       </div>
