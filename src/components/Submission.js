af3e1d02c0c025f8bad2c9f3938a79633394129e
Brandon Vernon <brandontvernon@gmail.com>
2018-06-02 19:45:46 -0500

    React-Bootstrap form

--- src/components/Submission.js
+++ src/components/Submission.js
@@ -1,129 +1,229 @@
 import React, { Component } from 'react';
+import {FormGroup, ControlLabel, FormControl, HelpBlock, FormExample} from 'react-bootstrap'
 import './Submission.css';
 
 class Submission extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
+  constructor(props, context) {
+    super(props, context);
 
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      value: ''
+    };
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
+  getValidationState() {
+    const length = this.state.value.length;
+    if (length > 10) return 'success';
+    else if (length > 5) return 'warning';
+    else if (length > 0) return 'error';
+    return null;
   }
 
-  handleSubmit(event) {
-    alert('A story was submitted: ' + this.state.value);
-    event.preventDefault();
+  handleChange(e) {
+    this.setState({ value: e.target.value });
   }
 
   render() {
     return (
-      <div className='container'>
-        <form className='form' onSubmit={this.handleSubmit}>
-          <div className='row'>
-            <div className='col'>
-              <label>
-                Headline
-                <input type="text" value={this.state.name} onChange={this.handleChange} />
-              </label>
-            </div>
-            <div className='col'>
-              <label>
-                Description
-                <input type="text" value={this.state.description} onChange={this.handleChange} />
-              </label>
-            </div>
-          </div>
-          <div className='row'>
-            <div className='col'>
-              <label>
-                Location Name
-                <input type="text" value={this.state.location_name} onChange={this.handleChange} />
-              </label>
-            </div>
-            <div className='col'>
-              <label>
-                Address
-                <input type="text" value={this.state.address} onChange={this.handleChange} />
-              </label>
-            </div>
-          </div>
-          <div className='row'>
-            <div className='col'>
-              <label>
-                Latitude
-                <input type="text" value={this.state.lat} onChange={this.handleChange} />
-              </label>
-            </div>
-            <div className='col'>
-              <label>
-                Longitude
-                <input type="text" value={this.state.long} onChange={this.handleChange} />
-              </label>
-            </div>
-          </div>
-          <div className='row'>
-            <div className='col'>
-              <label>
-                Drop Pin
-                <input type="text" value={this.state.drop_pin} onChange={this.handleChange} />
-              </label>
-            </div>
-            <div className='col'>
-              <label>
-                Tags
-                <input type="text" value={this.state.tags} onChange={this.handleChange} />
-              </label>
-            </div>
-          </div>
-          <div className='row'>
-            <div className='col'>
-              <label>
-                Collection
-                <input type="text" value={this.state.collection} onChange={this.handleChange} />
-              </label>
-            </div>
-            <div className='col'>
-              <label>
-                Time
-                <input type="text" value={this.state.timestamp} onChange={this.handleChange} />
-              </label>
-            </div>
-          </div>
-          <div className='row'>
-            <div className='col'>
-              <label>
-                Time End
-                <input type="text" value={this.state.timestamp_end} onChange={this.handleChange} />
-              </label>
-            </div>
-            <div className='col'>
-              <label>
-                Current Time
-                <input type="text" value={this.state.timestamp_created} onChange={this.handleChange} />
-              </label>
-            </div>
-          </div>
-          <div className='row'>
-            <div className='col'>
-              <label>
-                Sources
-                <input type="text" value={this.state.sources} onChange={this.handleChange} />
-              </label>
-            </div>
-            <div className='col'>
-              <label>
-                Your Name
-                <input type="text" value={this.state.owner} onChange={this.handleChange} />
-              </label>
-            </div>
-          </div>
-            <input type="submit" value="Submit" />
-        </form>
-      </div>
+      <form>
+        <FormGroup
+          controlId="formBasicText"
+          validationState={this.getValidationState()}
+        >
+          <ControlLabel>Headline</ControlLabel>
+          <FormControl
+            type="text"
+            value={this.state.name}
+            placeholder="Enter text"
+            onChange={this.handleChange}
+          />
+          <FormControl.Feedback />
+          <HelpBlock></HelpBlock>
+        </FormGroup>
+        <FormGroup
+          controlId="formBasicText"
+          validationState={this.getValidationState()}
+        >
+          <ControlLabel>Desription</ControlLabel>
+          <FormControl
+            type="text"
+            value={this.state.description}
+            placeholder="Enter text"
+            onChange={this.handleChange}
+          />
+          <FormControl.Feedback />
+          <HelpBlock></HelpBlock>
+        </FormGroup>
+        <FormGroup
+          controlId="formBasicText"
+          validationState={this.getValidationState()}
+        >
+          <ControlLabel>Location Name</ControlLabel>
+          <FormControl
+            type="text"
+            value={this.state.location_name}
+            placeholder="Enter text"
+            onChange={this.handleChange}
+          />
+          <FormControl.Feedback />
+          <HelpBlock></HelpBlock>
+        </FormGroup>
+        <FormGroup
+          controlId="formBasicText"
+          validationState={this.getValidationState()}
+        >
+          <ControlLabel>Address</ControlLabel>
+          <FormControl
+            type="text"
+            value={this.state.address}
+            placeholder="Enter text"
+            onChange={this.handleChange}
+          />
+          <FormControl.Feedback />
+          <HelpBlock></HelpBlock>
+        </FormGroup>
+        <FormGroup
+          controlId="formBasicText"
+          validationState={this.getValidationState()}
+        >
+          <ControlLabel>Latitude</ControlLabel>
+          <FormControl
+            type="text"
+            value={this.state.lat}
+            placeholder="Enter text"
+            onChange={this.handleChange}
+          />
+          <FormControl.Feedback />
+          <HelpBlock></HelpBlock>
+        </FormGroup>
+        <FormGroup
+          controlId="formBasicText"
+          validationState={this.getValidationState()}
+        >
+          <ControlLabel>Longitude</ControlLabel>
+          <FormControl
+            type="text"
+            value={this.state.long}
+            placeholder="Enter text"
+            onChange={this.handleChange}
+          />
+          <FormControl.Feedback />
+          <HelpBlock></HelpBlock>
+        </FormGroup>
+        <FormGroup
+          controlId="formBasicText"
+          validationState={this.getValidationState()}
+        >
+          <ControlLabel>Drop Pin</ControlLabel>
+          <FormControl
+            type="text"
+            value={this.state.drop_pin}
+            placeholder="Enter text"
+            onChange={this.handleChange}
+          />
+          <FormControl.Feedback />
+          <HelpBlock></HelpBlock>
+        </FormGroup>
+        <FormGroup
+          controlId="formBasicText"
+          validationState={this.getValidationState()}
+        >
+          <ControlLabel>Tags</ControlLabel>
+          <FormControl
+            type="text"
+            value={this.state.tags}
+            placeholder="Enter text"
+            onChange={this.handleChange}
+          />
+          <FormControl.Feedback />
+          <HelpBlock></HelpBlock>
+        </FormGroup>
+        <FormGroup
+          controlId="formBasicText"
+          validationState={this.getValidationState()}
+        >
+          <ControlLabel>Collection</ControlLabel>
+          <FormControl
+            type="text"
+            value={this.state.collection}
+            placeholder="Enter text"
+            onChange={this.handleChange}
+          />
+          <FormControl.Feedback />
+          <HelpBlock></HelpBlock>
+        </FormGroup>
+        <FormGroup
+          controlId="formBasicText"
+          validationState={this.getValidationState()}
+        >
+          <ControlLabel>Time</ControlLabel>
+          <FormControl
+            type="text"
+            value={this.state.timestamp}
+            placeholder="Enter text"
+            onChange={this.handleChange}
+          />
+          <FormControl.Feedback />
+          <HelpBlock></HelpBlock>
+        </FormGroup>
+        <FormGroup
+          controlId="formBasicText"
+          validationState={this.getValidationState()}
+        >
+          <ControlLabel>Time End</ControlLabel>
+          <FormControl
+            type="text"
+            value={this.state.timestamp_end}
+            placeholder="Enter text"
+            onChange={this.handleChange}
+          />
+          <FormControl.Feedback />
+          <HelpBlock></HelpBlock>
+        </FormGroup>
+        <FormGroup
+          controlId="formBasicText"
+          validationState={this.getValidationState()}
+        >
+          <ControlLabel>Current Time</ControlLabel>
+          <FormControl
+            type="text"
+            value={this.state.timestamp_created}
+            placeholder="Enter text"
+            onChange={this.handleChange}
+          />
+          <FormControl.Feedback />
+          <HelpBlock></HelpBlock>
+        </FormGroup>
+        <FormGroup
+          controlId="formBasicText"
+          validationState={this.getValidationState()}
+        >
+          <ControlLabel>Sources</ControlLabel>
+          <FormControl
+            type="text"
+            value={this.state.sources}
+            placeholder="Enter text"
+            onChange={this.handleChange}
+          />
+          <FormControl.Feedback />
+          <HelpBlock></HelpBlock>
+        </FormGroup><FormGroup
+          controlId="formBasicText"
+          validationState={this.getValidationState()}
+        >
+          <ControlLabel>Your Name</ControlLabel>
+          <FormControl
+            type="text"
+            value={this.state.owner}
+            placeholder="Enter text"
+            onChange={this.handleChange}
+          />
+          <FormControl.Feedback />
+          <HelpBlock></HelpBlock>
+        </FormGroup>
+      </form>
     );
   }
 }
