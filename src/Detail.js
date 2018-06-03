0b509e28cc974d6cf1bc309c9b7334dffb5a2387
Kenny Vo <kennyvo91@gmail.com>
2018-06-03 13:52:31 -0500

    detail modal working

--- src/Detail.js
+++ src/Detail.js
@@ -1,26 +1,48 @@
-import React from "react";
-import queryString from "query-string";
-import { Link } from "react-router-dom";
-import { distanceInWordsToNow, format } from "date-fns";
-import "./Detail.css";
+import React, {Component} from 'react';
+import { Modal, ModalHeader } from "react-bootstrap";
+import queryString from 'query-string';
+import {Link} from 'react-router-dom';
+import {distanceInWordsToNow, format} from 'date-fns'
+import './Detail.css'
 
-const Detail = ({ match, location, searchData, searchLocalPoints }) => {
+class Detail extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleClose() {
+    this.props.history.push("/");
+    this.setState({ show: false });
+  }
+
+  handleShow() {
+    this.setState({ show: true });
+  }
+
+  render() {
+
+  const { match, location, searchData, searchLocalPoints } = this.props;
   const { id, lat, long } = queryString.parse(location.search);
   const selectedPoint = searchData[id];
   if (!selectedPoint) return <div>Loading</div>;
+
   return (
     <div className="Detail">
+      <Modal show={true} onHide={this.handleClose}>
       <h2>{selectedPoint.name}</h2>
       <h4>{selectedPoint.description}</h4>
 
       <div className="stats">
         <div>
-          <b>Collection: </b>
-          {JSON.parse(selectedPoint.collection).map(coll => (
-            <Link to={`/?${queryString.stringify({ collection: coll })}`}>
-              {coll}
-            </Link>
-          ))}
+        <b>Collection: </b>
+        {JSON.parse(selectedPoint.collection).map(coll => (
+          <Link to={`/?${queryString.stringify({collection: coll})}`}>
+            {coll}
+          </Link>
+        ))}
         </div>
         <div>
           <b>Location Name: </b> {selectedPoint.location_name}
@@ -30,11 +52,10 @@ const Detail = ({ match, location, searchData, searchLocalPoints }) => {
           {selectedPoint.address}
         </div>
         <div>
-          <b>Date: </b>
-          {format(selectedPoint.timestamp, "MMM DD YYYY")}
+          <b>Date: </b>{format(selectedPoint.timestamp, 'MMM DD YYYY')}
         </div>
         <div className="citation">
-          <b>Source: </b>{" "}
+          <b>Source: </b>{' '}
           <a target="blank" href={selectedPoint.sources}>
             {selectedPoint.sources}
           </a>
@@ -54,8 +75,11 @@ const Detail = ({ match, location, searchData, searchLocalPoints }) => {
         <b>{distanceInWordsToNow(selectedPoint.timestamp_end)}</b>
       </div>*/}
       </div>
+    </Modal>
     </div>
-  );
-};
+  )
+}
+}
 
-export default Detail;
+
+export default Detail
